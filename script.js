4fae799e9a03a1d37f81e20b547ff888813dc7c7
Rishi Goyal <rishigoyal230@gmail.com>
2022-04-01 13:16:55 +0530

    added getResponse

--- script.js
+++ script.js
@@ -1,38 +1,55 @@
 let language = document.getElementById("selectBox");
-let code = document.getElementById("editor");
-let response;
+let codeEditor = document.getElementById("editor");
+let displayOutput = document.getElementById("outputScreen");
 
 function initialConfig() {
-	code.setAttribute("placeholder", "#Write your code here");
+	codeEditor.setAttribute("placeholder", "#Write your code here");
 }
 
 initialConfig();
 
 let compileButton = document.getElementById("compile");
-compileButton.setAttribute("onclick", "compileCode()");
+compileButton.addEventListener("click", compileCode);
 
 function compileCode() {
-    if (language.value == "null") {
-        alert("Please select a language");
-    }
+	if (language.value == "null") {
+		alert("Please select a language");
+	} else if (codeEditor.value == "") {
+		alert("Please write your code");
+	} else {
+		let request = new XMLHttpRequest();
+		let data = { code: codeEditor.value, langId: language.value };
+		let jsonData = JSON.stringify(data);
+		console.log(jsonData);
 
-    else if (code.value == "") { 
-        alert("Please write your code");
-    }
+		request.open("POST", "https://codequotient.com/api/executeCode");
+		// request.crossDomain = true;
+		request.setRequestHeader("Content-Type", "application/json");
+		request.send(jsonData);
 
-    let request = new XMLHttpRequest();
-    request.open("POST", "https://codequotient.com/api/executeCode");
-    // request.crossDomain = true;
-    request.setRequestHeader('Content-Type', 'application/json');
-    let codeToSend = JSON.stringify(code.value);
-    request.send(JSON.stringify({
-        code: codeToSend,
-        langId: language.value
-    }));
+		setTimeout(function () {
+			let response = JSON.parse(request.responseText);
+			console.log(response);
+			getResponse(response.codeId);
+		}, 5000);
+	}
+}
 
-    request.addEventListener("load", function () { 
-        response = JSON.parse(request.response);
-        
-        setTimeout(getResponse(), 5000);
-    });
+function getResponse(codeId) {
+	let request = new XMLHttpRequest();
+	request.open("GET", `https://codequotient.com/api/codeResult/${codeId}`);
+	request.send();
+
+	request.addEventListener("load", function (event) {
+		console.log(event.target.responseText);
+		let receivedData = JSON.parse(event.target.responseText);
+
+		let tempOutput = JSON.parse(receivedData.data);
+
+		if (tempOutput.output != "") {
+			displayOutput.innerHTML = tempOutput.output;
+		} else {
+			displayOutput.innerHTML = tempOutput.errors;
+		}
+	});
 }
